4c4dfe95a3adb53985c60053058f8f24bbceefa9
Joerg Peter Eggeling <no-spam-github@eggeling-hur.de>
2019-06-13 11:05:30 +0200

    Credentials used to log into the denic RRI interface

---
+++ authentication-default.php
@@ -0,0 +1,18 @@
+/* Credentials to connect to the denic RRI interface
+
+user: your handle registered at denic
+password_test: password used to connect to the denic test environment
+password_real: password used to connect to the denic production environment
+productive: boolean value to switch between test and production environment
+mailto: e-mail address to send status messages to
+from: sender address
+*/
+
+<?php
+$user = "DENIC-MEMBER_ID-NAME";
+$password_test = "password";
+$password_real = "password";
+$productive = true;
+$mailto = "user@bdomain.tld";
+$from = "From: user@bdomain.tld";
+?>
